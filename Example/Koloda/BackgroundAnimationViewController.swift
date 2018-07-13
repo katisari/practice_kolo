88d4cea1b33a38e26237529c7753175030c98392
ktjlee <ktjlee@umich.edu>
2018-07-13 14:40:44 -0700

    pic

--- Example/Koloda/BackgroundAnimationViewController.swift
+++ Example/Koloda/BackgroundAnimationViewController.swift
@@ -10,42 +10,77 @@ import UIKit
 import Koloda
 import pop
 
-private let numberOfCards: Int = 5
-private let frameAnimationSpringBounciness: CGFloat = 9
-private let frameAnimationSpringSpeed: CGFloat = 16
-private let kolodaCountOfVisibleCards = 2
-private let kolodaAlphaValueSemiTransparent: CGFloat = 0.1
-var image_present = 1
-var favorited_items: [Int] = []
+
 
 class BackgroundAnimationViewController: UIViewController {
+    private let frameAnimationSpringBounciness: CGFloat = 9
+    private let frameAnimationSpringSpeed: CGFloat = 16
+    private let kolodaCountOfVisibleCards = 2
+    private let kolodaAlphaValueSemiTransparent: CGFloat = 0.1
+    var image_present = 1
+    var favorited_items: [Int] = []
+    var json_result: [Any] = []
+    var item_dict: [NSDictionary] = []
+
 
     @IBOutlet weak var kolodaView: CustomKolodaView!
     
     //MARK: Lifecycle
     override func viewDidLoad() {
+        apiCall()
         super.viewDidLoad()
         kolodaView.alphaValueSemiTransparent = kolodaAlphaValueSemiTransparent
         kolodaView.countOfVisibleCards = kolodaCountOfVisibleCards
         kolodaView.delegate = self
         kolodaView.dataSource = self
         kolodaView.animator = BackgroundKolodaAnimator(koloda: kolodaView)
-        apiCall()
+        
         self.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
     }
     
     func apiCall(){
-        let url = URL(string: "https://openapi.etsy.com/v2/listings/active?includes=Images&api_key=samo6t9x98x9t58q0v2vcjhm")
+        let url = URL(string: "https://openapi.etsy.com/v2/listings/active?keywords=%22crazy_socks%22&limit=10&includes=Images:2&api_key=samo6t9x98x9t58q0v2vcjhm")
         let session = URLSession.shared
         let task = session.dataTask(with: url!, completionHandler: {
             data, response, error in
             do {
-                print("getting data..")
+//                print("getting data..")
                 if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments) as? NSDictionary {
-                    print(jsonResult)
+                    if let resultsArray = jsonResult["results"] as? [Any] {
+                        self.json_result = resultsArray
+                        for i in 0...9 {
+                            var main_price = ""
+                            var main_img_url = ""
+                            if let firstarr = resultsArray[i] as? NSDictionary {
+                                //                            print(firstarr)
+                                if let price = firstarr["price"] as? String {
+                                    main_price = price
+//                                    main_price = price
+                                }
+                                if let images = firstarr["Images"] as? [Any] {
+                                    if let arr = images[0] as? NSDictionary {
+                                        //
+                                        if let url_img = arr["url_570xN"] as? String {
+                                            main_img_url = url_img
+                                        }
+                                    }
+                                }
+                            }
+                            let dict:NSDictionary = ["price": main_price, "url": main_img_url] as NSDictionary
+                            self.item_dict.append(dict)
+                            
+                        }
+                        DispatchQueue.main.async {
+                            self.kolodaView.reloadData()
+                        }
+                        
+                        
+                        
+                    }
+                    
                 }
             } catch {
-                print("++++++")
+//                print("++++++")
                 print(error)
             }
         })
@@ -111,16 +146,34 @@ extension BackgroundAnimationViewController: KolodaViewDataSource {
     }
     
     func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
-        return numberOfCards
+        return item_dict.count
     }
     
     func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
 //        print(index)
         image_present = index
-//        let url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg")
-//        let data = try? Data(contentsOf: url!)
-//        return UIImageView(image: UIImage(data: data!))
-        return UIImageView(image: UIImage(named: "cards_\(index + 1)"))
+//        var image_url :String = ""
+//        if let firstarr = json_result[index] as? NSDictionary {
+//            print(firstarr)
+//            if let images = firstarr["Images"] as? [Any] {
+//                print("asds")
+//
+//                if let arr = images[0] as? NSDictionary {
+//
+//                    if let url_img = arr["url_570xN"] as? String {
+//                        print(url_img)
+//                        image_url = url_img
+//                    }
+//                }
+//            }
+//        }
+        
+//        print(getting_url!)
+        let url = URL(string: item_dict[index].value(forKey: "url") as! String)
+        
+        let data = try? Data(contentsOf: url!)
+        return UIImageView(image: UIImage(data: data!))
+//        return UIImageView(image: UIImage(named: "cards_\(index + 1)"))
         
         
     }
